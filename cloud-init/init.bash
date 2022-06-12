c035e1c2207e1e39b85b9efb0df5fc6e8aff2674
Skurhse Rage 🌆🌃🌌 <srage@its4logistics.com>
2022-06-12 02:41:26 -0500

    Checkpoint

--- cloud-init/init.bash
+++ cloud-init/init.bash
@@ -1,35 +1,136 @@
 #!/usr/bin/env bash
 
+set -Cefuxo pipefail
+
 # REQ: Provisions in cloud-init `runcmd`. <skr 2022-06-11>
 
-set -Cefuxo pipefail
+# TODO: Check checksums. <skr 2022-06-11>
+
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+mkdir /tmp/cloud-init
+cd $_
+
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
 arch=$(arch)
 
-bin=/usr/local/bin
-src=/usr/local/src
+etc=/etc
+lib=/var/lib
+local=/usr/local bin=$local/bin src=$local/src
 
 declare -A versions=(
   [minecraft_exporter]=0.11.2
   [node_exporter]=1.3.1
-  [paper]=''
-  [prometheus]=2.36.1
+  [quilt]=''
 )
 
-declare -A prometheus=(
-  [git]=https://github.com/prometheus/prometheus
-  [release]=prometheus-${versions[prometheus]}.linux-$arch
-  [checksum]=e5e555c84f04ccf20821fe907088e7ccecf418c88be3bd552b07c774d448d339
-  [etc]=/etc/prometheus
-  [lib]=/var/lib/prometheus
-  [owner]=prometheus
-  [group]=prometheus
-  [unit]=prometheus
-)
-prometheus[url]=${prometheus[git]}/releases/download/v${versions[prometheus]}/${prometheus[release]}.tar.gz
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
-prometheus_programs=(prometheus promtool)
-prometheus_configs=(consoles/ console_libraries/ prometheus.yml)
+download_release() {
+  for key in ${!release[@]}
+  do
+    declare $key=${release[$key]}
+  done
+
+  local name=$name-$version.linux-$arch 
+  local url=https://github.com/$owner/$repo/releases/downloads/v$version/$name.tar.gz
+
+  release[name]=$name
+  release[url]=$url
+  
+  wget $url | tar x -C $src
+}
+
+install_from_release() {
+  for entity in ${entities[@]}
+  do
+    cp -r ${release[name]}/$entity $dir
+    chown -R ${unit[owner]}:${unit[group]} $dir/$entity
+  done
+}
+
+install_dirs() {
+  for dir in ${dirs[@]}
+  do
+    mkdir $dir
+    chown ${unit[owner]}:${unit[group]} $dir
+  done
+}
+
+handle_systemctl() {
+  declare -n u=unit
+  systemctl daemon-reload
+  systemctl start $u
+  systemctl enable $u
+}
+
+configure_iptables() {
+  sudo iptables -A INPUT -p tcp -m state --state NEW --dport 25555 -j ACCEPT
+  # where 25555 is the port
+  # you do that for every ports
+  # and then
+  sudo iptables-save
+  # and also
+  sudo dpkg-reconfigure iptables-persistent
+
+  # of course to block everything
+  sudo iptables -P INPUT DROP
+  sudo iptables -P OUTPUT DROP
+  sudo iptables -P FORWARD DROP
+}
+
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+name=prometheus
+declare -A release=([owner]=$name [repo]=$name [version]=2.36.1)
+declare -A dirs=([etc]=/etc/$name [lib]=/var/lib/$name)
+declare -A unit=([owner]=$name [group]=$name [name]=$name)
+declare -a programs=($name promtool)
+declare -a configs=(consoles console_libraries name.yml)
+declare -a exporters=(node minecraft)
+
+install_release
+install_dirs
+entities=(${programs[@]}) dir=$bin install_from_release
+entities=(${programs[@]}) dir=${dirs[etc]} install_from_release
+handle_systemctl
+
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
+for exporter in ${exporters[@]}
+do
+  install_exporter $exporter
+done
+
+cc=$c
+c=node_exporter
+
+declare -A ${c}_release=(
+  [owner]=$cc
+  [repo]=$c
+)
+set_github_release
+
+declare -A ${c}_unit=(
+  [owner]=$c
+  [group]=$c
+  [name]=$c
+)
+
+c=minecraft_exporter
+
+declare -A ${c}_release=(
+  [owner]=dirien
+  [repo]=minecraft-$cc-exporter
+)
+set_github_release
+
+declare -A ${c}_node_exporter_unit=(
+  [owner]=node_exporter
+  [group]=node_exporter
+  [name]=node_exporter
+)
 
 declare -A node_exporter=(
   [git]=https://github.com/prometheus/node_exporter
@@ -40,46 +141,8 @@ declare -A node_exporter=(
 )
 node_exporter[url]=${node_exporter[git]}/releases/download/v${versions[node_exporter]}/${node_exporter[release]}.tar.gz
 
-exporters=(node minecraft)
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
-main() {
-  cd /tmp
-  mkdir cloud-init
-  cd $_
-
-  configure_iptables
-  
-  install_prometheus
-  
-  for exporter in ${exporters[@]}
-  do
-    install_exporter $exporter
-  done
-
- install_paper
-}
-
-configure_iptables() {
-  iptables -I INPUT -j ACCEPT
-}
-
-install_prometheus() {
-  declare -n service=prometheus
-  declare -n programs=prometheus_programs
-  declare -n configs=prometheus_configs
-
-  mkdir ${service[etc]} mkdir ${service[lib]}
-
-  install_to_source
-
-  pushd $src
-  for $entity in ${programs[@]} ${configs[@]} ${p[lib]}; do
-    install_to_bin
-  done
-  popd
-
-  handle_systemctl
-}
 
 install_exporter() {
   declare -n p=$1_exporter
@@ -94,26 +157,8 @@ install_exporter() {
   handle_systemctl ${e[unit]}
 }
 
-install_to_source() {
-  wget ${p[url]} | tar x -C $src
-}
-
-install_to_bin() {
-    declare e=$entity
-
-    cp -r ${p[release]}/$e $bin
-    chown -R ${p[owner]}:${p[group]} $bin/$element
-
-    cp -r ${p[release]}/$e ${p[etc]} 
-    chown -R ${p[owner]}:${p[group]} ${p[etc]}/$e
-}
 
 
-handle_systemctl() {
-  systemctl daemon-reload
-  systemctl start $1
-  systemctl enable $1
-}
 
 curl -sSL https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/node_exporter-$NODE_EXPORTER_VERSION.linux-$ARCH.tar.gz | tar -xz
 cp node_exporter-$NODE_EXPORTER_VERSION.linux-$ARCH/node_exporter /usr/local/bin
@@ -125,17 +170,9 @@ chown minecraft_exporter:minecraft_exporter /usr/local/bin/minecraft-exporter
 systemctl start minecraft-exporter.service
 systemctl enable minecraft-exporter.service
 
+# ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
 mkdir -p /minecraft
 sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
 service sshd restart
 systemctl restart fail2ban
-URL="https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/136/downloads/paper-1.18.1-136.jar"
-curl -sLSf $URL > /minecraft/server.jar
-echo "eula=true" > /minecraft/eula.txt
-mv /tmp/server.properties /minecraft/server.properties
-chmod a+rwx /minecraft
-systemctl restart minecraft.service
-systemctl enable minecraft.service
-}
-
-main "$@"
