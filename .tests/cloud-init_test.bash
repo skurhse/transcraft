89058725e8ddb3282aa1fe2db6cf40d9167ee376
Skurhse Rage 🌆🌃🌌 <srage@its4logistics.com>
2022-06-12 23:50:44 -0500

    Update

--- .tests/cloud-init_test.bash
+++ .tests/cloud-init_test.bash
@@ -10,7 +10,5 @@ path=$(realpath "$0"); init="$(dirname $path)/../cloud-init/init.yaml"
 
 name='transcraft' cpus='1' mem='4GB' disk='32GB'
 
-if multipass launch --name "$name" -c$cpus -m$mem -d$disk --cloud-init "$init"
-then
-  multipass delete --purge foo
-fi
+multipass launch --name "$name" -c$cpus -m$mem -d$disk --cloud-init "$init"
+multipass exec transcraft -- cat /var/log/cloud-init-output.log
