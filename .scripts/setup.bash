06e63211d7ff4fe03294e7ccb9277b36022e9e44
Skurhse Rage 🌆🌃🌌 <srage@its4logistics.com>
2022-06-17 23:33:03 -0500

    Rallypoint

--- .scripts/setup.bash
+++ .scripts/setup.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# REQ: Installs dev tools on macOS Montery. <skr 2002-06-12>
+# REQ: Installs dev tools on macOS Montery. <skr 2002-06-15>
+
+# TODO: Handle ~/.bash_profile setup. <>
 
 set -o errexit
 set -o noclobber
@@ -9,19 +11,70 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-declare -A packages=(
-    azure-cli
-    bash
-    github
+local=/usr/local
+src=$local/bin
+src=$local/src
+sudo mkdir -p $src
 
+ brew_formulae=(
+  'azure-cli'
+  'bash'
+  'gh'
+  'gnu-tar'
+  'yq'
 )
 
-brew update
+ brew_casks=(
+  'multipass'
+)
 
-for package in ${packages[@]}; do brew install $package; done
+ launchpad_releases=(
+  'cloud-init'
+)
 
-brew install azure-cli
-brew install bash
-brew install github
-brew install --cask multipass
+main() {
+  update_brew
+  install_brew_formulae
+  install_brew_casks
 
+  install_launchpad_releases
+}
+
+update_brew() {
+  brew update
+}
+
+install_brew_formulae() {
+  for formula in ${brew_ormulae[@]}
+  do
+    brew install $formula
+  done
+}
+
+install_brew_casks() {
+  for cask in ${brew_casks[@]}
+  do
+    brew install --cask $cask
+  done
+}
+
+# TODO: Figure-out how to properly query for latest. <skr>
+# Install to multipass virtual machine
+install_launchpad_releases() {
+  for release in "${launchpad_releases[@]}"
+  do
+    resource=$(
+      curl -LSfs "https://launchpad.net/$release/trunk/+rdf" | \
+      xq -r .'"rdf:RDF"."lp:ProductSeries"."lp:release"[0]."lp:ProductRelease"."lp:specifiedAt"."@rdf:resource"'
+    )
+    version=$(
+      curl -LSfs "https://launchpad.net$resource" | \
+      xq -r '."rdf:RDF"."lp:ProductRelease"."lp:version"'
+    )
+
+    curl -LSfs "https://launchpad.net/$release/trunk/$version/+download/$release-$version.tar.gz" | \
+    sudo tar xz -C $src
+  done
+}
+
+main
